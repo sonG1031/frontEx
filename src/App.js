664e482cfc57164e869552a671f76d9169eed3c0
Minsub Song <minsub1031@gmail.com>
2023-04-19 12:04:56 +0900

    code (2)

--- src/App.js
+++ src/App.js
@@ -3,23 +3,23 @@ import './App.css';
 
 function App() {
   const code="01"
-  let nationality = "None"
-
-  if (code === "01") {
-    nationality = "korea"
-  }
-  else if (code === "02") {
-    nationality = "China"
-  }
-  else if (code === "03") {
-    nationality = "Japan"
-  }
-  else {
-    nationality = "..."
+  let getNationality=(code)=> {
+    if (code === "01") {
+      return "Korea"
+    }
+    else if (code === "02") {
+      return "China"
+    }
+    else if (code === "03") {
+      return "Japan"
+    }
+    else {
+      return "..."
+    }
   }
   return (
     <div>
-      <h1>Minsub's nationality is {nationality}</h1>
+      <h1>Minsub's nationality is {getNationality(code)}</h1>
     </div>
   );
 }
