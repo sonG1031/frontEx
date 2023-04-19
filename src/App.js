637d50511badee099b1a8a2c065d4b228b6d69c9
Minsub Song <minsub1031@gmail.com>
2023-04-19 11:42:00 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const hobby = ["Sports", "Music", "Movie"];
-  const hobbyList = hobby.map(hobby => <li>{hobby}</li>);
+  const hobbyList = hobby.map((hobby, index) => <li key={index}>{hobby}</li>);
   console.log(hobbyList);
   return (
     <ul>{hobbyList}</ul>
