07e53bc801c5aed43f2abbd9e7a35f39e87438e8
Minsub Song <minsub1031@gmail.com>
2023-04-21 12:11:08 +0900

    2023-04-21

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
-// import logo from './logo.svg';
-import './App.css';
+import { Component} from "react";
 
-function App() {
-  const code="01"
-  let getNationality=(code)=> {
-    if (code === "01") {
-      return "Korea"
-    }
-    else if (code === "02") {
-      return "China"
-    }
-    else if (code === "03") {
-      return "Japan"
-    }
-    else {
-      return "..."
+// class App extends Component {
+//   render() {
+//     const userName = "Minsub";
+//     return <h1>{userName}</h1>
+//   }
+// }
+
+// const App = () => {
+//   // 상태와 상태 설정함수 정의
+//   const [userName, setUserName] = useState('Minsub');
+  
+//   // 함수정의
+//   const handleClickDosa = () => setUserName("Dosa");
+//   const handleClickSado = () => setUserName("Sado");
+//   return (
+//     <div>
+//       <h1>Hello {userName}</h1>
+//       <button onClick={handleClickDosa}>Dosa</button>
+//       <button onClick={handleClickSado}>Sado</button>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userName : "Minsub"
     }
   }
-  return (
-    <div>
-      <h1>Minsub's nationality is {getNationality(code)}</h1>
-    </div>
-  );
+  handleClickDosa = () => {
+    console.log("Dosa");
+    this.setState({userName: "Dosa"});
+  };
+  handleClickSado = () => {
+    console.log("Sado");
+    this.setState({userName: "Sado"});
+  };
+
+  render() {
+    const {userName} = this.state;
+    return (
+      <div>
+        <h1>{userName}</h1>
+        <button onClick={this.handleClickDosa}>Dosa</button>
+        <button onClick={this.handleClickSado}>Sado</button>
+      </div>
+    ) 
+  }
 }
 
 export default App;
