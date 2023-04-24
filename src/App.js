42e41d199e4dba500dbc534a7d56d88101528f7b
Minsub Song <minsub1031@gmail.com>
2023-04-24 12:01:25 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,41 @@
-import { Component} from "react";
+import { useState } from "react";
 
-// class App extends Component {
-//   render() {
-//     const userName = "Minsub";
-//     return <h1>{userName}</h1>
-//   }
-// }
+const App = () => {
+    var itemDataArray = [
+      {
+        name:"CPU",
+        price:"462984",
+        quantity:1,
+      },
+      {
+        name:"MainBoard",
+        price:"112053",
+        quantity:1,
+      },
+      {
+        name:"Memory",
+        price:"79608",
+        quantity:2,
+      }
+    ]
+    
+    const [items, setItems] = useState(itemDataArray);
+    const totalprice = items.reduce(function(sum,item) {
+      return sum + item.price * item.quantity
+    }, 0);
 
-// const App = () => {
-//   // 상태와 상태 설정함수 정의
-//   const [userName, setUserName] = useState('Minsub');
-  
-//   // 함수정의
-//   const handleClickDosa = () => setUserName("Dosa");
-//   const handleClickSado = () => setUserName("Sado");
-//   return (
-//     <div>
-//       <h1>Hello {userName}</h1>
-//       <button onClick={handleClickDosa}>Dosa</button>
-//       <button onClick={handleClickSado}>Sado</button>
-//     </div>
-//   );
-// }
-
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      userName : "Minsub"
-    }
-  }
-  handleClickDosa = () => {
-    console.log("Dosa");
-    this.setState({userName: "Dosa"});
-  };
-  handleClickSado = () => {
-    console.log("Sado");
-    this.setState({userName: "Sado"});
-  };
-
-  render() {
-    const {userName} = this.state;
     return (
-      <div>
-        <h1>{userName}</h1>
-        <button onClick={this.handleClickDosa}>Dosa</button>
-        <button onClick={this.handleClickSado}>Sado</button>
-      </div>
-    ) 
-  }
-}
+      <>
+        <ul>
+        {items.map((item)=> (
+          <li key={item.name}>
+            {item.name}:{item.price} x {item.quantity} = {item.price * item.quantity}원
+          </li>
+        ))}
+       </ul>
+       <p>합계: {totalprice}원</p>
+      </>
+    )
+};
 
 export default App;
